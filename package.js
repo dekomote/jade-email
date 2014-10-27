b80f8cf1e2c40fd04053b1a296a273734e60df3a
Erkka Hänninen <erkka.hanninen@gmail.com>
2014-10-27 19:44:24 +0000

    package.js update

--- package.js
+++ package.js
@@ -2,21 +2,21 @@ Package.describe({
   name: 'erkkaha:jade-email',
   summary: 'Server side email templates with jade',
   version: '1.0.0',
-  git: ' /* Fill me in! */ '
+  git: 'https://github.com/erkkaha/jade-email.git'
 });
 Npm.depends({jade: "1.7.0"});
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.4');
   api.use(['underscore'], 'server');
-  api.addFiles('jade-email.js');
+  api.addFiles('jade-email.js', 'server');
   api.export('EmailTemplate', ['server'])
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('erkkaha:jade-email');
-  api.addFiles('jade-email-tests.js');
+  api.addFiles('jade-email-tests.js', 'server');
 });
 
 
